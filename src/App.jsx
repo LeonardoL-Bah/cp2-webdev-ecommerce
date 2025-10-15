8b11732d55e0598677ce62ee16b1b167935cfb0b
Leonardo Lopes Pereira <leonardolopespereira7@gmail.com>
2025-10-14 22:05:32 -0300

    salvar progresso 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import Navbar from "../components/Navbar";
+import Navbar from "./components/Navbar";
 
 export default function App(){
     return(
         <div className="min-h-screen bg-black text-white">
             <Navbar/>
             <main className="p-6">
-                <Outlet />
             </main>
         </div>
     )
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import App from "/src/App.jsx";
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        children:[
+            {index:true,element:<Home/>}
+        ]
+    }
+])
+
+export default router;
